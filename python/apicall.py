a24d4702af3bba8541f5a1346cbc621b812f18b7
Katie Ford <katherine.ford@dreamhost.com>
2019-08-20 12:33:33 -0700

    updates to apicall

--- python/apicall.py
+++ python/apicall.py
@@ -23,9 +23,6 @@ class ApiCall:
         """
         args => required. a dictionary of arguments
         user => optional. a user for this apicall
-
-        Why is command separate from the other args?
-        Metrics recording
         """
         self.args = args
 
@@ -36,36 +33,30 @@ class ApiCall:
             return self._get_key_creds()
         raise Exception('Uknown credential type')
 
-    def _get_ouath_creds(self):
-        with open('etc/auth/client' + self.api_name) as f:
-            client_id = f.readline().strip()
-            secret = f.readline().strip()
+    def _get_oauth_creds(self):
+        oauth_creds = self._get_client_info()
         user_info = self._get_user_info()
-        return {
-            'client_id': client_id,
-            'client_secret': secret,
-            'username': user_info[0],
-            'password': user_info[1],
-            'code': user_info[2],
-            'token': user_info[3]
-        }
+        oauth_creds.update({'access_token': user_info})
+        return oauth_creds
 
     def _get_key_creds(self):
         with open('../etc/auth/client/' + self.api_name) as f:
             key = f.readline().strip()
         return key
 
-    def _get_user_info(self):
-        filename = '../etc/auth/client/' + api_name
-        if (self.user):
-            filename = filename + self.user
-        with open(filename) as f:
-            username = f.readline().strip()
-            password = f.freadline().strip()
-            code = f.readline().strip()
-            token = f.readline().strip()
-        return (username, password, code, token)
+    def _get_client_info(self):
+        with open('../etc/auth/client/' + self.api_name) as f:
+            client_id = f.readline().strip()
+            secret = f.readline().strip()
+        return {'client_id': client_id, 'secret': secret}
 
+    def _get_user_info(self):
+        filename = '../etc/auth/user/' + self.api_name
+        if (self.user):
+            filename = filename + '/' + self.user
+        with open(filename) as f:
+            code = f.readline().strip()
+        return code
 
     def _build_request(self):
         raise Exception('Method needs to be subclassed')
@@ -73,12 +64,12 @@ class ApiCall:
     def place_call(self):
         self._build_request()
         self.response = request.urlopen(self.request)
+        print(self.response)
         if self.call_successful():
             self._deserialize_response()
         else:
             self._handle_error()
         self._log()
-        self._emit_metric()
         return
 
     def call_successful(self):
@@ -88,6 +79,7 @@ class ApiCall:
         from the api.  Which may or may not contain an error
         """
         successful_codes = [200, 201, 202, 203, 204, 205, 206, 207, 208, 226]
+        print(self.response.status)
         if (self.response.status in successful_codes):
             return True
         else:
@@ -106,7 +98,9 @@ class ApiCall:
         you are using.  This method will likely need to be subclassed
         """
         if not self.data:
+            print("No data in response")
             self.error = 'No data in response'
+            self._notify()
 
     def _notify(self):
         pass
@@ -116,9 +110,6 @@ class ApiCall:
         # Sometimes this is going to be a matter of security, technology or
         # teams It also lets us separate out the different uses, so each does
         # its purpose well instead of trying to do everything.
-        pass
-
-    def _emit_metric(self):
         pass
 
     """
